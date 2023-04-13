f1fcb79cc35260f6dcb8a6a3e314b94a95e1f093
H-goto16 <h.goto.engineer@gmail.com>
2023-04-13 14:02:27 +0900

    feat: add file

---
+++ src/1_4.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  enum koyomi {January = 1, Febuary, March, April, May, June, July, August, September, October, November, December} Calender;
+  return 0;
+}
